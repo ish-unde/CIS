28492c2621ea67e24587542059c73b05f3065fa7
Angela Appiah <aappiah7@jh.edu>
2025-10-05 18:47:58 -0400

    forgot to commit

--- PA1/main.py
+++ PA1/main.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from math_utils_cart import Point3D, Rotation, Frame, find_rigid_transform, calculate_centroid
 from file_io import read_points_from_file
@@ -5,6 +6,13 @@ from testing import test_basic_math_operations, test_kabsch_algorithm
 
 def main():
     print("Tests for PA1 \n")
+
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <input_file>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    frames, d_points, a_points, c_points = read_points_from_file(filename)
     #tests 
     test_basic_math_operations()
     test_kabsch_algorithm()
@@ -16,28 +24,28 @@ if __name__ == "__main__":
 
 # part 4a. 
 
-def find_fd(frames):
+def find_fd(frames, d_points):
     """ 
     computing the transformation FD between optical tracker and EM tracker coordinate
     """
 
-    # read all the d_j points from file
+    all_optical_D = []
+    all_d_points = []
 
-    d_points = read_points_from_file("data/points_d.txt") # not correct
-    d_points = [Point3D(*point) for point in d_points]
+    for frame in frames:
+        all_optical_D.extend(frame.D)
+        all_d_points.extend(frame.d_points)
 
-    optical_A = frames[1].points
 
-    optical_D = frames[0].points
-
-    F_d = find_rigid_transform(optical_D, d_points)
+    F_d = find_rigid_transform(all_d_points, all_optical_D)
 
     return F_d
 
 
 # helper to read files 
-def read_frames_from_file(filename):
+def read_file(filename):
     frames = []
+
 
     with open(filename, 'r') as file:
 
@@ -48,24 +56,24 @@ def read_frames_from_file(filename):
         num_a_points = int(header_parts[1])
         num_c_points = int(header_parts[3])
         num_frames = int(header_parts[2])
+        
 
 
         for _ in range(num_frames):
-            d_points = []
-            a_points = []
-            c_points = []
+            d_j = []
+            a_j = []
+            c_j = []
             for _ in range(num_d_points):
                 line = file.readline().strip()
                 x, y, z = map(float, line.split(','))
-                d_points.append(Point3D(x, y, z))
+                d_j.append(Point3D(x, y, z))
             for _ in range(num_a_points):
                 line = file.readline().strip()
                 x, y, z = map(float, line.split(','))
-                a_points.append(Point3D(x, y, z))
+                a_j.append(Point3D(x, y, z))
             for _ in range(num_c_points):
                 line = file.readline().strip()
                 x, y, z = map(float, line.split(','))
-                c_points.append(Point3D(x, y, z))
-            frames.append(Frame(d_points, a_points, c_points))
+                c_j.append(Point3D(x, y, z))
+            frames.append(Frame(d_j, a_j, c_j))
     return frames 
-
